5b3ce1220fddad32e2206ab10a60421fad36a0d0
abhay.saini <abhay.saini@cnvrg.io>
2022-09-28 12:23:50 +0000

    modified batch files

--- batch/prerun.sh
+++ batch/prerun.sh
@@ -1,4 +1,5 @@
-sudo apt-get install -y libgl1-mesa-dev
-sudo apt-get install -y libfreetype6-dev
+apt-get update
+apt-get install -y libgl1-mesa-dev
+apt-get install -y libfreetype6-dev
 pip uninstall -y pillow
 pip install --no-cache-dir pillow
