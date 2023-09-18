91b46d21dee1acd7997a66760662861b3cd885fd
Abhaycnvrg <abhay.saini@cnvrg.io>
2023-09-18 15:02:54 +0000

    changed requirements and prerun

--- inference/prerun.sh
+++ inference/prerun.sh
@@ -19,7 +19,9 @@
 #
 # SPDX-License-Identifier: MIT
 
-sudo apt-get install -y libgl1-mesa-dev
-sudo apt-get install -y libfreetype6-dev
+apt-get update
+apt-get install -y libmagic1
+apt-get install -y libglib2.0-0
+apt-get install -y ffmpeg libsm6 libxext6
 pip uninstall -y pillow
 pip install --no-cache-dir pillow
